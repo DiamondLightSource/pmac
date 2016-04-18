5133d6b65adf6123841e5ea52b6d7ec76154eabe
Alan Greer <alan.greer@diamond.ac.uk>
2016-04-18 13:40:16 +0100

    Added instructions for building calc, busy and motor modules for CI.

--- ci/build-synapps-deps.sh
+++ ci/build-synapps-deps.sh
@@ -11,5 +11,23 @@ echo "EPICS_BASE=/usr/lib/epics" > asyn-R4-26/configure/RELEASE
 #echo "EPICS_LIBCOM_ONLY=YES" >> asyn-R4-26/configure/CONFIG_SITE
 make -C asyn-R4-26/
 
+wget https://github.com/epics-modules/calc/archive/R3-6-1.tar.gz
+tar -zxf R3-6-1.tar.gz
+echo "EPICS_BASE=/usr/lib/epics" > calc-R3-6-1/configure/RELEASE
+make -C calc-R3-6-1/
+
+wget -nv http://www.aps.anl.gov/bcda/synApps/tar/busy_R1-6-1.tar.gz
+tar -zxf busy_R1-6-1.tar.gz
+echo "ASYN=`pwd`/asyn-R4-26" > busy-1-6-1/configure/RELEASE 
+echo "EPICS_BASE=/usr/lib/epics" >> busy-1-6-1/configure/RELEASE
+make -C busy-1-6-1/
+
+wget -nv http://www.aps.anl.gov/bcda/synApps/motor/tar/motorR6-9.tar.gz
+tar -zxf motorR6-9.tar.gz
+echo "ASYN=`pwd`/asyn-R4-26" > motorR6-9/configure/RELEASE
+echo "BUSY=`pwd`/busy-1-6-1" >> motorR6-9/configure/RELEASE
+echo "EPICS_BASE=/usr/lib/epics" >> motorR6-9/configure/RELEASE
+make -C motorR6-9/
+
 cd ..
 
--- ci/configure.sh
+++ ci/configure.sh
@@ -6,10 +6,6 @@ set -e
 # Generate the configure/RELEASE.local and configure/CONFIG_SITE.linux-x86_64.Common
 # with the details of where to find various external libraries.
 echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
-echo "HDF5=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "HDF5_LIB=/usr/lib"                     >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "XML2_INCLUDE=-I/usr/include/libxml2"   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST=/usr"                            >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST_LIB=/usr/lib"                    >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST_INCLUDE=-I/usr/include"          >> configure/CONFIG_SITE.linux-x86_64.Common
@@ -18,6 +14,9 @@ echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
 
 echo "ASYN=`pwd`/external/asyn-R4-26"        >> configure/RELEASE.local
+echo "CALC=`pwd`/external/calc-R3-6-1"       >> configure/RELEASE.local
+echo "BUSY=`pwd`/external/busy-1-6-1"        >> configure/RELEASE.local
+echo "MOTOR=`pwd`/external/motorR6-9"        >> configure/RELEASE.local
 
 echo "======= configure/RELEASE.local ========================================="
 cat configure/RELEASE.local
