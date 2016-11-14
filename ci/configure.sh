896fd99b9b706954afbe9dee0a12a29321ed9521
Alan Greer <alan.greer@diamond.ac.uk>
2016-11-14 15:01:07 +0000

    Unset SSH for travis build.

--- ci/configure.sh
+++ ci/configure.sh
@@ -9,6 +9,7 @@ echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
 echo "BOOST=/usr"                            >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST_LIB=/usr/lib/x86_64-linux-gnu"   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST_INCLUDE=-I/usr/include"          >> configure/CONFIG_SITE.linux-x86_64.Common
+echo "SSH="                                  >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HOST_OPT=NO"                           >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
