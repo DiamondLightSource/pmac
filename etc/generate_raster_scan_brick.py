56882613cf4deba10ae0b0d23d951fd4c1f39e43
Giles Knap <giles.knap@diamond.ac.uk>
2017-06-28 10:23:51 +0100

    add separate sample IOC for lab geobrick

---
+++ etc/generate_raster_scan_brick.py
@@ -0,0 +1,88 @@
+import sys
+import os, time
+from optparse import OptionParser
+try:
+    from pkg_resources import require
+    require('six')
+    require('cothread')
+    require('matplotlib')
+
+    require('h5py-swmr')
+
+    import cothread
+    from cothread.catools import *
+    import numpy
+    from numpy import *
+
+    import h5py
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+except:
+    pass
+
+def main():
+    print('caput -c -w 1 PMAC_TEST:GB1:A:Resolution -0.0005')
+    print('caput -c -w 1 PMAC_TEST:GB1:A:Offset 0.0')
+    print('caput -c -w 1 PMAC_TEST:GB1:B:Offset 0.0')
+    print('caput -c -w 1 PMAC_TEST:GB1:B:Resolution -0.0005')
+    print('caput -c -w 1 PMAC_TEST:GB1:C:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:Z:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:Y:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:V:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:B:UseAxis 1')
+    print('caput -c -w 1 PMAC_TEST:GB1:A:UseAxis 1')
+    print('caput -c -w 1 PMAC_TEST:GB1:U:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:W:UseAxis 0')
+    print('caput -c -w 1 PMAC_TEST:GB1:X:UseAxis 0')
+
+    no_of_rows = 80
+    y_val = -4.0
+    x_range = 4.0
+    x_delta = x_range / 60.0
+    x_start = -12.0 - x_delta
+    print('caput -a PMAC_TEST:GB1:A:Positions {0}'.format(no_of_rows * 62)),
+    for index in range(no_of_rows):
+      for pts in range(63):
+        if index%2 == 0:
+          print('{0}'.format(x_start + x_delta * float(pts))),
+        else:
+          print('{0}'.format(x_start + x_delta * float(62-pts))),
+        
+    sys.stdout.flush()
+    print('')
+        
+    print('caput -a PMAC_TEST:GB1:B:Positions {0}'.format(no_of_rows * 62)),
+    for index in range(no_of_rows):
+      for pts in range(63):
+        print('{0}'.format(-4.0 - (float(index)*0.05))),
+        
+    sys.stdout.flush()
+    print('')
+        
+    print('caput -a PMAC_TEST:GB1:ProfileTimeArray {0}'.format(no_of_rows * 62)),
+    print('{0}'.format(3000)),
+    for index in range(no_of_rows):
+      for pts in range(62):
+        print('{0}'.format(100)),
+      print('{0}'.format(4000)),
+      
+    sys.stdout.flush()
+    print('')
+       
+    print('caput -a PMAC_TEST:GB1:VelocityMode {0}'.format(no_of_rows * 62)),
+    print('{0}'.format(2)),
+    for index in range(no_of_rows):
+      for pts in range(61):
+        print('{0}'.format(0)),
+      print('{0}'.format(1)),
+      print('{0}'.format(2)),
+      
+    sys.stdout.flush()
+    print('')
+       
+    print('caput -c -w 1 PMAC_TEST:GB1:ProfileNumPoints {0}'.format(no_of_rows * 62))
+    print('caput -c -w 1 PMAC_TEST:GB1:ProfileBuild 1')
+
+if __name__ == "__main__":
+    main()
+
