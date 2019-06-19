bc31f3edd78da84ee6e75176c180dca9a1242239
Giles Knap <giles.knap@diamond.ac.uk>
2019-06-19 14:32:58 +0100

    add a malcolm scan for testing

---
+++ etc/malcolm/blocks/__init__.py
@@ -0,0 +1,8 @@
+from malcolm.yamlutil import make_block_creator, check_yaml_names
+
+pmac_runnable_block = make_block_creator(__file__, "pmac_runnable_block.yaml")
+pmac_test_scan = make_block_creator(__file__, "pmac_test_scan.yaml")
+
+
+__all__ = check_yaml_names(globals())
+
---
+++ test/test_malcolm/test_trajectories.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from test.brick.trajectory import Trajectory
+from test.brick.movemonitor import MoveMonitor
+from datetime import datetime
+from test.brick.testbrick import TestBrick, DECIMALS
+from test.test_system.trajectories import trajectory_quick_scan
+import os
+
+
+class TestTrajectories(TestCase):
+    def test_spiral(self):
+        pass
