efeb34054922317bafd666208b7ed69c2559688d
Giles Knap <giles.knap@diamond.ac.uk>
2019-07-16 16:05:42 +0100

    improved system tests for malcolm trajectories

--- test/brick/testbrick.py
+++ test/brick/testbrick.py
@@ -73,6 +73,15 @@ class TBrick(MyBrick):
         self.trajectory = Trajectory(self.pv_root)
 
         if init:
+            # reset all real mres
+            mres_pvs = [axis.mres for axis in r.values()]
+            mres_val = [0.001] * len(mres_pvs)
+            ca.caput(mres_pvs, mres_val, wait=True, timeout=10)
+            ca.caput(["BRICK1:M1.MRES", "BRICK1:M2.MRES"], [1, 1], wait=True)
+            # higher resolution on 7 and 8 for clipper trajectories
+            ca.caput(self.m7.mres, 0.00001)
+            ca.caput(self.m8.mres, 0.00001)
+
             # make all motors fast to speed up tests
             # (and encourage race conditions)
             acc_pvs = [axis.acc for axis in a.values()]
@@ -85,16 +94,6 @@ class TBrick(MyBrick):
 
             # also make motors speed, acceleration in motion programs fast
             self.send_command('i117,8,100=10 i116,8,100=200')
-
-            # reset all real mres
-            mres_pvs = [axis.mres for axis in r.values()]
-            mres_val = [0.001] * len(mres_pvs)
-            ca.caput(mres_pvs, mres_val, wait=True, timeout=10)
-            ca.caput(["BRICK1:M1.MRES", "BRICK1:M2.MRES"], [1, 1], wait=True)
-            # higher resolution on 7 and 8 for clipper trajectories
-            # ca.caput(self.m7.mres, 0.00001)
-            # ca.caput(self.m8.mres, 0.00001)
-
             # reset all UEIP
             e_pvs = [axis.pv_use_encoder for axis in r.values()]
             e_val = [0] * len(mres_pvs)
--- test/test_malcolm/test_trajectories.py
+++ test/test_malcolm/test_trajectories.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import cothread.catools as ca
+from cothread import Sleep
 import numpy as np
 from dls_pmaclib.dls_pmacremote import PmacEthernetInterface
 from dls_pmaclib.pmacgather import PmacGather
@@ -13,14 +14,10 @@ from scanpointgenerator import SpiralGenerator, CompoundGenerator, \
 from .plot_trajectories import plot_velocities
 from ..brick.testbrick import TBrick
 
-SAMPLE_RATE = 10  # how many between each point to gather
+SAMPLE_RATE = 30  # how many between each point to gather
 BRICK_CLOCK = 5000  # servo loop speed in Hz
 FUDGE = .2  # no. secs to add to gather time as a safe buffer
 AXES = [7, 8]  # always use axes x, y which are mapped to 7, 8
-
-# for debugging set to True to just plot the trajectory without
-# trying to download it to the pmac
-SKIP_RUN = True
 
 
 class TestTrajectories(TestCase):
@@ -34,6 +31,7 @@ class TestTrajectories(TestCase):
         self.gather_points = None
         self.start_x = 0
         self.start_y = 0
+        self.step_time = 0
         self.m_res = []
         self.axes = []
         self.total_time = 0
@@ -41,22 +39,20 @@ class TestTrajectories(TestCase):
         for axis in AXES:
             self.axes.append("stage{}".format(axis))
 
-        self.make_malcolm()
         self.brick_connect()
 
     def tearDown(self) -> None:
         self.proc.stop()
-        if not SKIP_RUN:
-            self.pmac.disconnect()
-        pass
+        self.pmac.disconnect()
 
-    def setup_brick(self, xs, ys, xa, ya):
-        self.test_brick.m7.set_speed(xs)
-        self.test_brick.m8.set_speed(ys)
+    def setup_brick_malcolm(self, xv, yv, xa, ya):
+        self.test_brick.m7.set_speed(xv)
+        self.test_brick.m8.set_speed(yv)
         self.test_brick.m7.set_acceleration(xa)
         self.test_brick.m8.set_acceleration(ya)
 
-    def make_malcolm(self):
+        Sleep(.1)
+
         # create a malcolm scan from a YAML definition
         yaml_file = Path(
             __file__).parent / '../../etc/malcolm/PMAC-ML-TEST-01.yaml'
@@ -86,11 +82,12 @@ class TestTrajectories(TestCase):
         self.m_res.append(float(ca.caget(self.test_brick.m7.mres)))
         self.m_res.append(float(ca.caget(self.test_brick.m8.mres)))
 
-    def do_a_scan(self, gen: CompoundGenerator):
+    def do_a_scan(self, gen: CompoundGenerator, skip_run=False):
         # configure the Malcolm scan
         self.scan_block.configure(gen)
         self.start_x = self.test_brick.m7.pos
         self.start_y = self.test_brick.m8.pos
+        self.step_time = gen.duration
         self.total_time = np.sum(
             self.test_brick.trajectory.getProfileTimeArray())
         # time array is in microseconds
@@ -102,7 +99,7 @@ class TestTrajectories(TestCase):
         samples = (self.total_time + FUDGE) * samples_per_sec
         self.pmac_gather.gatherConfig(AXES, samples, ticks_per_sample)
 
-        if not SKIP_RUN:
+        if not skip_run:
             # start gathering and scanning
             self.pmac_gather.gatherTrigger(wait=False)
             self.scan_block.run()
@@ -121,7 +118,7 @@ class TestTrajectories(TestCase):
                 egu_points = np.multiply(c.scaledData, self.m_res[i])
                 self.gather_points.append(egu_points)
 
-    def plot_scan(self, title, step_time):
+    def plot_scan(self, title):
         p = np.insert(np.array(self.traj_block.positionsX.value), 0,
                       self.start_x), \
             np.insert(np.array(self.traj_block.positionsY.value), 0,
@@ -131,7 +128,7 @@ class TestTrajectories(TestCase):
             np.insert(np.array(self.traj_block.userPrograms.value), 0, 0)
         print('trajectory arrays:-\n', p)
 
-        plot_velocities(p, title=title, step_time=step_time,
+        plot_velocities(p, title=title, step_time=self.step_time,
                         overlay=self.gather_points)
 
     def test_spiral(self):
@@ -142,33 +139,62 @@ class TestTrajectories(TestCase):
         gen = CompoundGenerator([s], [], [], step_time)
         gen.prepare()
 
-        self.setup_brick(xs=100, ys=100, xa=.2, ya=.2)
+        self.setup_brick_malcolm(xv=100, yv=100, xa=.2, ya=.2)
 
         self.do_a_scan(gen)
-        self.plot_scan('Live Spiral', step_time)
+        self.plot_scan('Live Spiral')
 
     def test_raster(self):
-        step_time = .1
-        xs = LineGenerator(self.axes[0], "mm", 0, 10, 3)
+        self.lines(False)
+
+    def test_snake(self):
+        self.lines(True)
+
+    def lines(self, snake=False):
+        step_time = .2
+        xs = LineGenerator(self.axes[0], "mm", 0, 10, 3, alternate=snake)
         ys = LineGenerator(self.axes[1], "mm", 0, 8, 3)
 
         gen = CompoundGenerator([ys, xs], [], [], step_time)
         gen.prepare()
 
-        self.setup_brick(xs=100, ys=100, xa=.5, ya=.5)
+        self.setup_brick_malcolm(xv=100, yv=100, xa=.5, ya=.5)
 
         self.do_a_scan(gen)
-        self.plot_scan('Live Raster', step_time)
+        self.plot_scan('Live Snake')
 
-    def test_snake(self):
-        step_time = .2
-        xs = LineGenerator(self.axes[0], "mm", 0, 10, 3, alternate=True)
-        ys = LineGenerator(self.axes[1], "mm", 0, 2, 3)
+    def test_high_acceleration(self):
+        # this system test performs the same trajectory as the malcolm unit
+        # test pmacchildpart_test.test_turnaround_overshoot
+        self.Interpolation_checker(17, 1, .1, .2)
 
-        gen = CompoundGenerator([ys, xs], [], [], step_time)
+    def Interpolation_checker(self, xv=200., yv=400., xa=1., ya=80.,
+                              skip=True, snake=False):
+        xs = LineGenerator("stage7", "mm", 0, 5, 4, alternate=snake)
+        ys = LineGenerator("stage8", "mm", 0, 1, 2)
+
+        gen = CompoundGenerator([ys, xs], [], [], 0.15)
         gen.prepare()
 
-        self.setup_brick(xs=100, ys=100, xa=.5, ya=5)
+        self.setup_brick_malcolm(xv=xv, yv=yv, xa=xa, ya=ya)
 
-        self.do_a_scan(gen)
-        self.plot_scan('Live Snake', step_time)
+        # todo try this with a fast brick and remove skip_run
+        self.do_a_scan(gen, skip_run=skip)
+        self.plot_scan('X Overshoot test xv={} yv={} xa={} ya={}'.format(
+            xv, yv, xa, ya))
+
+    def test_profile_point_interpolation(self):
+        # test combinations of velocity profile points
+        # x=6 and y=3 combined = 7
+        # todo add some checks to see if these have overrun
+        #  just need to see if x,y exceed expected bounds
+        self.Interpolation_checker()
+        self.scan_block.reset()
+        # x=6 and y=4 combined = 8
+        self.Interpolation_checker(ya=1)
+        self.scan_block.reset()
+        # x=4 and y=3 combined = 5
+        self.Interpolation_checker(snake=True)
+        self.scan_block.reset()
+        # x=3 and y=4 combined = 5
+        self.Interpolation_checker(ya=.01, snake=True)
